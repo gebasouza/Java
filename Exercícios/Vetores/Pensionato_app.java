7f41171a5241a67ffe6cb5e996b1dc1c4b5a0a84
Gerson Souza <57300340+gebasouza@users.noreply.github.com>
2023-07-24 15:12:37 -0300

    Update Pensionato_app.java

--- Exercícios/Vetores/Pensionato_app.java
+++ Exercícios/Vetores/Pensionato_app.java
@@ -1,4 +1,14 @@
+/*
+ * A dona de um pensionato possui dez quartos para alugar para estudantes, sendo esses 
+ quartos identificados pelos números 0 a 9.
+Fazer um programa que inicie com todos os dez quartos vazios, 
+e depois leia uma quantidade N representado o número de estudantes que
+vão alugar quartos (N pode ser de 1 a 10). Em seguida, registre o aluguel dos N estudantes.
+Para cada registro de aluguel, informar o nome e email do estudante, bem com qual dos quartos vago. 
+Ao final, seu programa deve imprimir um relatório de todas ocupações do pensionato, por ordem de quarto, conforme exemplo.
 
+ * 
+ */
 
 import java.util.Locale;
 import java.util.Scanner;
